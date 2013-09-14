973ec1f202b748949d3082e03850e39c7f5f754b
chrifpa <chrifpa@gmail.com>
2013-09-14 16:50:57 +0200

    moved the message sending

--- lib/readp.ex
+++ lib/readp.ex
@@ -1,35 +1,44 @@
 defmodule Readp do
-    def parse(url, readability_key, pid // Kernel.self) do
+
+    def xml_quote(str) do
+        str |> to_string |> String.replace("&", "&amp;") |> String.replace("<", "&lt;") |> String.replace(">", "&gt;") |> String.replace("\"", "&quot;")
+    end
+
+    def parse(url, readability_key) do
         case :httpc.request 'https://readability.com/api/content/v1/parser?token=' ++ to_char_list(readability_key) ++ '&url=' ++ to_char_list(url) do
             {:ok, {{_,_,'OK'}, _, resp}} ->
                 case JSEX.decode to_string(resp), [{:labels, :atom}] do
-                    {:ok, parsed}   ->
-                        pid <- {:ok, parsed[:title], parsed[:content]}
-                    _               -> pid <- {:error}
+                    {:ok, parsed}   -> {:ok, parsed[:title], parsed[:content]}
+                    _               -> {:error}
                 end
             err ->
-                pid <- err
+                err
         end
     end
 
     defp accept :ok, list do
         receive do
-            {:ok, title, content} ->
-                accept :ok, list ++ [[{:title, title}, {:content, content}]]
+            {:ok, item} ->
+                accept :ok, list ++ [item]
             _ ->
-                {:end?, list}
+                {:ok, list}
             after 1000 ->
                 {:ok, list}
         end
     end
 
 
-    def parse_multiple(urllist, readability_key) do
+    def parse_multiple(itemlist, readability_key, pid // self) do
         :inets.start
         :ssl.start
 
-        self = Kernel.self
-        Enum.map urllist, fn url -> parse url, readability_key, self end
+        Enum.map itemlist, fn item ->
+            case parse item[:link], readability_key do
+                {:ok, title, content} ->
+                    pid <- {:ok, [title: xml_quote(title), summary: xml_quote(content <> "<hr>" <> item[:summary]), link: xml_quote(item[:link]), clients: item[:clients]]}
+                _ -> {:error}
+                end
+        end
 
         accept :ok, []
 
